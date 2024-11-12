e61d647591e1bd53c3e0b01d6037d327f42f12d1
Donald <donumeh54@gmail.com>
2024-11-12 08:02:27 +0100

    simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Simple helper function
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    index_page: returns the page range for a page
+
+    page: the number of page
+    page_size: the total page size agreed upon
+
+    Return:
+        tuple(int int): holding the range of index to return
+    """
+    return tuple(((page - 1) * page_size, page_size * page))
+
+
+if __name__ == "__main__":
+    res = index_range(1, 7)
+    print(type(res))
+    print(res)
+
+    res = index_range(page=3, page_size=15)
+    print(type(res))
+    print(res)
