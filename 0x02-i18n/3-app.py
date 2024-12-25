8ff10d509a1b4691dee2b8129c3d87511a457e0b
Donald <donumeh54@gmail.com>
2024-12-25 01:30:04 +0100

    parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""
+Basic Flask app
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from typing import List
+
+
+app: Flask = Flask(__name__)
+
+
+class Config:
+    """
+    Config for flask application
+    """
+
+    LANGUAGES: List = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+
+
+app.config.from_object(Config)
+babel: Babel = Babel(app)
+
+
+@app.route("/", strict_slashes=True)
+def index() -> str:
+    """
+    Home Page
+    """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
